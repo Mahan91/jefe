77f05bfdbe5c26c9fcff935fb2ac4b6afbed876c
Anthony Mangano <amangano@largus.fr>
2019-09-17 17:10:54 +0200

    refactor(ui): Remove useless controllers

--- app/controllers/application.js
+++
@@ -1,8 +0,0 @@
-import Controller from '@ember/controller';
-import { computed } from '@ember/object';
-
-export default Controller.extend({
-  inApp: computed('currentPath', function () {
-    return this.get('currentPath').indexOf('data-set') === 0;
-  }),
-});
