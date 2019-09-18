df4345dc6f4b8eaf490f7ab86a1897305ae8cb01
Anthony Mangano <amangano@largus.fr>
2019-09-18 19:11:32 +0200

    test(ui) Add Computed filteredCategories unit test

--- tests/unit/controllers/data-set/referential/makes-test.js
+++ tests/unit/controllers/data-set/referential/makes-test.js
@@ -1,12 +1,49 @@
-import { module, test } from 'qunit';
-import { setupTest } from 'ember-qunit';
+import { expect } from 'chai';
+import { describe, it } from 'mocha';
+import { setupTest } from 'ember-mocha';
+import EmberObject from '@ember/object';
 
-module('Unit | Controller | data-set/referential/makes', function(hooks) {
-  setupTest(hooks);
+describe.only('Unit | Controller | data-set/referential/makes', function() {
+  setupTest();
 
-  // Replace this with your real tests.
-  test('it exists', function(assert) {
-    let controller = this.owner.lookup('controller:data-set/referential/makes');
-    assert.ok(controller);
+  describe('Computed filteredCategories', function() {
+    const testCases = [
+      {
+        message: 'No categories selected',
+        condition: [false, false],
+        result: ['category1', 'category2'],
+      },
+      {
+        message: 'One category selected',
+        condition: [true, false],
+        result: ['category1'],
+      },
+      {
+        message: 'All categories selected',
+        condition: [true, true],
+        result: ['category1', 'category2'],
+      },
+    ];
+
+    testCases.forEach((testCase) => {
+      it(testCase.message, function() {
+        const modelTest = EmberObject.create({
+          categories: [
+            EmberObject.create({
+              name: "category1",
+              showCategory: testCase.booleans[0]
+            }),
+            EmberObject.create({
+              name: "category2",
+              showCategory: testCase.booleans[1]
+            }),
+          ],
+        });
+        let controller = this.owner.lookup('controller:data-set/referential/makes');
+        controller.set('model', modelTest);
+
+        expect(controller.get('filteredCategories').mapBy('name')).to.members(testCase.result);
+      })
+    })
   });
 });
