05a6c4b88d7d02cfb15885d3b5380d114bbf3209
Anthony Mangano <amangano@largus.fr>
2019-09-24 15:42:07 +0200

    test(unit): add selectedCategories computed property

--- tests/unit/controllers/data-set/referential/make-test.js
+++ tests/unit/controllers/data-set/referential/make-test.js
@@ -1,13 +1,51 @@
 import { expect } from 'chai';
 import { describe, it } from 'mocha';
 import { setupTest } from 'ember-mocha';
+import EmberObject from '@ember/object';
 
 describe.only('Unit | Controller | data-set/referential/make', function() {
   setupTest();
 
-  // Replace this with your real tests.
-  it('exists', function() {
-    let controller = this.owner.lookup('controller:data-set/referential/make');
-    expect(controller).to.be.ok;
+  describe('Computed | selectedCategories', function() {
+    const testCases = [
+      {
+        message: 'No categories selected',
+        condition: [false, false],
+        result: ['category1', 'category2'],
+      },
+      {
+        message: 'One category selected',
+        condition: [true, false],
+        result: ['category1'],
+      },
+      {
+        message: 'All categories selected',
+        condition: [true, true],
+        result: ['category1', 'category2'],
+      },
+    ];
+
+    testCases.forEach((testCase) => {
+      it(testCase.message, function() {
+        const modelTest = EmberObject.create({
+          make: {
+            categories: [
+              EmberObject.create({
+                name: "category1",
+                isSelected: testCase.condition[0]
+              }),
+              EmberObject.create({
+                name: "category2",
+                isSelected: testCase.condition[1]
+              }),
+            ],
+          },
+        });
+        let controller = this.owner.lookup('controller:data-set/referential/make');
+        controller.set('model', modelTest);
+
+        expect(controller.get('selectedCategories').mapBy('name')).to.members(testCase.result);
+      })
+    })
   });
 });
